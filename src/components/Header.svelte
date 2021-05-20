23f6c5f1eaaee71edd635619c5fc31ecf2036cd8
SciDroid <scidroidgames@gmail.com>
2021-05-20 16:34:45 -0500

    add header and hero

---
+++ src/components/Header.svelte
@@ -0,0 +1,45 @@
+<header class="flex p-4">
+    <h1>
+        <a class="font-normal text-3xl" href="https://scidroid.me/">SciDroid</a>
+    </h1>
+    <nav class="flex justify-around">
+        <ul class="flex">
+            <li>
+                <a
+                    class="text-lg font-light p-2"
+                    href="https://scidroid.me/posts"
+                    >Posts</a
+                >
+            </li>
+            <li>
+                <a
+                    class="text-lg font-light p-2"
+                    href="https://scidroid.me/projects"
+                    >Projects</a
+                >
+            </li>
+        </ul>
+        <ul class="flex">
+            <li>
+                <a class="p-2" href="https://dev.to/scidroid"
+                    ><i class="fab fa-dev"></i
+                ></a>
+            </li>
+            <li>
+                <a class="p-2" href="https://github.com/scidroid"
+                    ><i class="fab fa-github"></i
+                ></a>
+            </li>
+            <li>
+                <a class="p-2" href="https://twitter.com/scidroid"
+                    ><i class="fab fa-twitter"></i
+                ></a>
+            </li>
+            <li>
+                <a class="p-2" href="mailto:scidroid@scidroid.me"
+                    ><i class="far fa-envelope"></i
+                ></a>
+            </li>
+        </ul>
+    </nav>
+</header>
---
+++ src/components/Hero.svelte
@@ -0,0 +1,17 @@
+<section class="hero p-10 rounded-sm border-gray-400">
+    <h2
+        class="text-4xl sm:text-6xl lg:text-7xl leading-none font-extrabold tracking-tight text-gray-900 mt-10 mb-8 sm:mt-14 sm:mb-10"
+    >
+        I'm a <a
+            class="text-light-blue-600 font-semibold"
+            style="box-shadow: rgb(255, 255, 255) 0px -0.166667em 0px 0px inset, rgb(186, 230, 253) 0px -0.333333em 0px 0px inset;"
+            >Web Semi-Fullstack</a
+        >
+        Developer and
+        <a
+            class="text-light-blue-600 font-semibold"
+            style="box-shadow: rgb(255, 255, 255) 0px -0.166667em 0px 0px inset, rgb(186, 230, 253) 0px -0.333333em 0px 0px inset;"
+            >Data Scientist</a
+        > in progress.
+    </h2>
+</section>
